25d20b0deafc751f55c52cd87e1d6aaa7e10594d
Tatiane <thathy23@gmail.com>
2025-05-02 23:51:12 -0300

    comentario com outra forma de resolver exercicio 50.js

--- exercicios_js/exercicio50.js
+++ exercicios_js/exercicio50.js
@@ -1,11 +1,19 @@
 //50 - Crie um script que declare uma função que receba um array de strings como parâmetro
 //  e retorne um novo array contendo apenas as strings que têm exatamente 4 caracteres.
 
-function escolhe4Caracteres(str){
-    return str.filter(item => item.length == 4)
+const array = ['cao', 'gato', 'lua','abcd', '1635474584', '1234'];
+
+const escolhe4Caracteres = (str) => {
+    return console.log(str.filter(item => item.length === 4 ));
 }
 
-const array = ['cao', 'gato', 'lua','abcd', '1635474584', ' 1234'];
-const arrayCom4 = escolhe4Caracteres(array);
+escolhe4Caracteres(array);
 
-console.log(arrayCom4)
+// function escolhe4Caracteres(str){
+//     return str.filter(item => item.length == 4)
+// }
+
+// const array = ['cao', 'gato', 'lua','abcd', '1635474584', ' 1234'];
+// const arrayCom4 = escolhe4Caracteres(array);
+
+// console.log(arrayCom4)
